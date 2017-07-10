a3e647a20d1ac780468925e50139417432cbc2c1
myztajay <myztajay@hotmail.com>
2017-07-10 18:19:10 -0400

    Styled basic landing page

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express =  require('express');
 const app = express();
 
-
-
-
+app.use(express.static(__dirname + '/src'))
 app
   .get('/',(req,res)=>{
     res.sendfile(__dirname + '/src/index.html');
