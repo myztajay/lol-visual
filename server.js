584999ca4d985cfd9d6fafac5b125d9b0a0a9888
myztajay <myztajay@hotmail.com>
2017-07-10 17:20:54 -0400

    Restructured routes and added dash page

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express =  require('express');
 const app = express();
 
-// app.get('/', (req, res)=>{
-//   res.send('Hello Jessy');
-// })
 
-app.use(express.static(__dirname ));
+
+
+app
+  .get('/',(req,res)=>{
+    res.sendfile(__dirname + '/src/index.html');
+  })
+  .get('/:name', (req,res)=>{
+  res.sendfile(__dirname + '/src/dash.html')
+  })
 
 const server = app.listen(3000, ()=>{
   const host = server.address().address;
   const port = server.address().port;
-})
+});
