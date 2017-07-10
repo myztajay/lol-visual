918cb7c9886de7630ff5ab6c0c881028b11b34e9
myztajay <myztajay@hotmail.com>
2017-07-10 15:46:20 -0400

    Added express, Now serving index.html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express =  require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-  res.send('Hello Jessy');
-})
+// app.get('/', (req, res)=>{
+//   res.send('Hello Jessy');
+// })
+
+app.use(express.static(__dirname ));
 
 const server = app.listen(3000, ()=>{
   const host = server.address().address;
